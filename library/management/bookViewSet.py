04b41dcb0d46d3bb99cca6ef6481cc9d945ea84d
Pragati Ranjan <pragatiranjan@pgsMAc.local>
2024-01-23 10:17:09 +0530

    added most popular book analysis

--- library/management/bookViewSet.py
+++ library/management/bookViewSet.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from .models import Book
+from .models import Book, Reservation
+from django.db.models import Count
 from .serializers import BookSerializer, ReservationSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
@@ -17,6 +18,11 @@ class BookViewSet(viewsets.ModelViewSet):
         reservations = obj.reservations.all()
         serializer = ReservationSerializer(reservations, many=True, context={'request': request})
         return Response(data=serializer.data)
+    
+    @action(detail=False, methods=['GET'])
+    def mostPopularBooks(self, request, *args, **kwargs):
+        most_popular_books = Reservation.objects.values('book__title', 'book__author', 'book__id').annotate(reservation_count=Count('id')).order_by('-reservation_count')
+        return Response(data= most_popular_books)
 
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
